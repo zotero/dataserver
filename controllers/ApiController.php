ffa902833466384c3a27b7b49c6df74449807296
Dan Stillman <dstillman@zotero.org>
2019-08-10 06:13:48 -0400

    Allow routes to waive an API key for non-GET requests

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -290,7 +290,7 @@ class ApiController extends Controller {
 				// Explicit auth request or not a GET request
 				//
 				// /users/<id>/keys is an exception, since the key is embedded in the URL
-				if ($this->method != "GET" && $this->action != 'keys') {
+				if ($this->method != "GET" && $this->action != 'keys' && empty($extra['noauth'])) {
 					$this->e403('An API key is required for write requests.');
 				}
 				
