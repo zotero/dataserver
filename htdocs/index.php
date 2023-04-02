40fa96a553fb0f3edcad1dbe795d9aaeb0d26b65
Dan Stillman <dstillman@zotero.org>
2023-04-01 22:13:36 -0400

    Log full request URL in debug output

--- htdocs/index.php
+++ htdocs/index.php
@@ -40,7 +40,7 @@ $extra = !empty($params['extra']) ? $params['extra'] : array();
 
 // Attempt to load controller
 $controllerFile = Z_ENV_CONTROLLER_PATH . $directory . $controllerName . 'Controller.php';
-Z_Core::debug($_SERVER['REQUEST_METHOD'] . " to " . Z_ENV_SELF);
+Z_Core::debug($_SERVER['REQUEST_METHOD'] . " to " . $_SERVER['REQUEST_URI']);
 Z_Core::debug("Controller is $controllerFile");
 
 if (file_exists($controllerFile)) {
