911aaa0b48e704bfd761b422f0dd117a35c70e87
Dan Stillman <dstillman@zotero.org>
2024-01-16 00:52:10 -0500

    Add purgeStorageUploadQueue event

--- misc/events.sql
+++ misc/events.sql
@@ -1,4 +1,5 @@
-DROP EVENT purgeOldKeys;
+DROP EVENT IF EXISTS purgeOldKeys;
+DROP EVENT IF EXISTS purgeStorageUploadQueue;
 
 delimiter |
 
@@ -9,6 +10,14 @@ CREATE EVENT purgeOldKeys
         BEGIN
             DELETE FROM `keys` WHERE lastSeen < NOW() - INTERVAL 2 YEAR;
             DELETE FROM keyAccessLog WHERE timestamp < NOW() - INTERVAL 2 YEAR;
+        END |
+
+CREATE EVENT purgeStorageUploadQueue
+    ON SCHEDULE
+        EVERY 1 DAY
+    DO
+        BEGIN
+            DELETE FROM storageUploadQueue WHERE time < NOW() - INTERVAL 1 DAY;
         END |
 
 delimiter ;
