zotero/dataserver

commit 5ee4cc9097142010361b26f90d25ad5c57935497

Don't discard integer path parts that aren't integers
Fixes a 500 being returned instead of a 400 if a string is passed for a
userID instead of a number

https://groups.google.com/g/zotero-dev/c/2lxLdcF_g0U/m/v6B7DbBKEAAJ

controllers/GroupsController.php
@@ -292,6 +292,13 @@ class GroupsController extends ApiController {
 			$this->e403();
 		}
 		
+		if (!is_numeric($this->scopeObjectID)) {
+			$this->e400("Invalid group ID", Z_ERROR_INVALID_INPUT);
+		}
+		if (!is_numeric($this->objectID)) {
+			$this->e400("Invalid user ID", Z_ERROR_INVALID_INPUT);
+		}
+		
 		$groupID = $this->scopeObjectID;
 		$userID = $this->objectID;
 		

controllers/ApiController.php
@@ -318,7 +318,7 @@ class ApiController extends Controller {
 		
 		// Get object user
 		if (isset($this->objectUserID)) {
-			if (!$this->objectUserID) {
+			if (!$this->objectUserID || !is_numeric($this->objectUserID)) {
 				$this->e400("Invalid user ID", Z_ERROR_INVALID_INPUT);
 			}
 			
@@ -352,7 +352,7 @@ class ApiController extends Controller {
 		}
 		// Get object group
 		else if (isset($this->objectGroupID)) {
-			if (!$this->objectGroupID) {
+			if (!$this->objectGroupID || !is_numeric($this->objectGroupID)) {
 				$this->e400("Invalid group ID", Z_ERROR_INVALID_INPUT);
 			}
 			// Make sure group exists

include/mvc/Router.inc.php
@@ -54,10 +54,13 @@ class Router {
 					if ($p[0]) {
 						switch ($p[0]) {
 						case 'i':
-							if (!is_numeric($part)) {
+							if ($part === false) {
 								continue 2;
 							}
-							$part = (int) $part;
+							// Cast to integer if it's supposed to be an integer
+							if (is_numeric($part)) {
+								$part = (int) $part;
+							}
 							break;
 							
 						default:

tests/remote/tests/API/3/GeneralTest.php
@@ -4,9 +4,9 @@
     
     This file is part of the Zotero Data Server.
     
-    Copyright © 2013 Center for History and New Media
-                     George Mason University, Fairfax, Virginia, USA
-                     http://zotero.org
+    Copyright © 2025 Corporation for Digital Scholarship
+                     Vienna, VA, USA
+                     https://www.zotero.org
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -38,6 +38,15 @@ class GeneralTests extends APITests {
 	public static function tearDownAfterClass(): void {
 		parent::tearDownAfterClass();
 		API::userClear(self::$config['userID']);
+	}
+	
+	
+	public function test_should_return_400_if_string_passed_as_userID() {
+		$response = API::userGet(
+			"foo",
+			"items"
+		);
+		$this->assert400($response);
 	}
 	
 	

commit abe8956061c0eab9f89f8d3bf6b99190a05ffe6d

Fix `POST /groups/[groupID]/users` breakage from 5ee4cc9097

controllers/GroupsController.php
@@ -295,9 +295,6 @@ class GroupsController extends ApiController {
 		if (!is_numeric($this->scopeObjectID)) {
 			$this->e400("Invalid group ID", Z_ERROR_INVALID_INPUT);
 		}
-		if (!is_numeric($this->objectID)) {
-			$this->e400("Invalid user ID", Z_ERROR_INVALID_INPUT);
-		}
 		
 		$groupID = $this->scopeObjectID;
 		$userID = $this->objectID;